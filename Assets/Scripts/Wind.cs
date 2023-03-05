7d31f6b34f4c47c73f3fb62d2ba04869ed0e938e
jennakampe <71042710+jennakampe@users.noreply.github.com>
2023-03-05 16:46:17 +0100

    Working on Wind

--- Assets/Scripts/Wind.cs
+++ Assets/Scripts/Wind.cs
@@ -4,51 +4,52 @@ using UnityEngine;
 
 public class Wind : MonoBehaviour
 {
-    //private BoxCollider2D bc;
+    private BoxCollider2D bc;
 
-    //public float windSpeed;
-    //void Start()
-    //{
-    //    bc = gameObject.GetComponent<BoxCollider2D>();
-    //    bc.isTrigger = true;
-    //}
+    public float windSpeed;
+    void Start()
+    {
+        bc = gameObject.GetComponent<BoxCollider2D>();
+        bc.isTrigger = true;
+    }
 
-    //void OnTriggerEnter2D(Collider2D other)
-    //{
-    //    if (other.gameObject.CompareTag("Bird"))
-    //    {
-    //        Rigidbody2D rbOther = other.gameObject.GetComponent<Rigidbody2D>();
-    //        if (rbOther != null)
-    //        {
-    //            rbOther.gravityScale = windSpeed;
-    //        }
-    //    }
-    //}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Rigidbody2D rbOther = other.gameObject.GetComponent<Rigidbody2D>();
+            if (rbOther != null)
+            {
+                rbOther.gravityScale = windSpeed;
+            }
+        }
+    }
 
-    //void OnTriggerExit2D(Collider2D other)
-    //{
-    //    if (other.gameObject.CompareTag("Bird"))
-    //    {
-    //        Rigidbody2D rbOther = other.gameObject.GetComponent<Rigidbody2D>();
-    //        if (rbOther != null)
-    //        {
-    //            rbOther.gravityScale = 1f;
-    //        }
-    //    }
-    //}
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Rigidbody2D rbOther = other.gameObject.GetComponent<Rigidbody2D>();
+            if (rbOther != null)
+            {
+                rbOther.gravityScale = 1f;
+            }
+        }
+    }
+
 
 
     //alternative maybe better way below???
 
-    void OnTriggerStay(Collider other)
-    {
-        Debug.Log("Object is in trigger");
-        Vector3 position = transform.position;
-        Vector3 targetPosition = other.transform.position;
-        Vector3 direction = targetPosition - position;
-        direction.Normalize();
-        int moveSpeed = 10;
-        other.transform.position += direction * moveSpeed * Time.deltaTime;
+    //void OnTriggerStay2D(Collider2D other)
+    //{
+    //    Debug.Log("Object is in trigger");
+    //    Vector3 position = transform.position;
+    //    Vector3 targetPosition = other.transform.position;
+    //    Vector3 direction = targetPosition - position;
+    //    direction.Normalize();
+    //    int moveSpeed = 10;
+    //    other.transform.position += direction * moveSpeed * Time.deltaTime;
 
-    }
+    //}
 }
