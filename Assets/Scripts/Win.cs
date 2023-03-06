e59daf9628ae4bcac0a19156d78a49cd9bff16cc
jennakampe <71042710+jennakampe@users.noreply.github.com>
2023-03-06 23:09:49 +0100

    updated spiders

--- Assets/Scripts/Win.cs
+++ Assets/Scripts/Win.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Victory : MonoBehaviour
+public class Win : MonoBehaviour
 {
     private BoxCollider2D bc;
 
