8628e5fe37ce3bdb04c54b740f29364ad0b3e4a2
helenbranch <124572208+helenbranch@users.noreply.github.com>
2023-03-06 17:56:29 +0100

    Revert "Frames make sense now!"
    This reverts commit 3cde285905ab05feb2b92f9ba432f2b2449cc868, reversing
    changes made to 125218cdc2c07a7808ade8e97a71573f58aa5237.

---
+++ Assets/Scripts/Wind.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+    private BoxCollider2D bc;
+
+    public float windSpeed;
+    void Start()
+    {
+        bc = gameObject.GetComponent<BoxCollider2D>();
+        bc.isTrigger = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Rigidbody2D rbOther = other.gameObject.GetComponent<Rigidbody2D>();
+            if (rbOther != null)
+            {
+                rbOther.gravityScale = windSpeed;
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Rigidbody2D rbOther = other.gameObject.GetComponent<Rigidbody2D>();
+            if (rbOther != null)
+            {
+                rbOther.gravityScale = 1f;
+            }
+        }
+    }
+
+    //void OnTriggerStay2D(Collider2D other)
+    //{
+    //    Debug.Log("Object is in trigger");
+    //    Vector3 position = transform.position;
+    //    Vector3 targetPosition = other.transform.position;
+    //    Vector3 direction = targetPosition - position;
+    //    direction.Normalize();
+    //    int moveSpeed = 10;
+    //    other.transform.position += direction * moveSpeed * Time.deltaTime;
+
+    //}
+}
